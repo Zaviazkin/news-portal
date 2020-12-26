927a86d47495997f56d25260f74cab5e655c26aa
Roman <zavyazkin8@gmail.com>
2020-12-27 04:32:40 +0600

    add time bish

--- middleware/time.js
+++ middleware/time.js
@@ -1,10 +1,11 @@
 const timeMiddleware = function (req, res, next) {
   let time = new Date()
-  let tima = tim.getHours()
+  let tima = time.getHours()
  time.setHours(tima+6)
     req.requestTime = time
     next();
   };
+
   module.exports = {
       timeMiddleware
   }
